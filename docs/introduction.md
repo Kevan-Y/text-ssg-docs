17d7bfa39df30f5ea2364090c3da34f43368150c
Kevan-Y <58233223+Kevan-Y@users.noreply.github.com>
2021-10-28 19:36:10 -0400

    Change docusaurus config:    - Change default route    - Change theme config    - Remove unused components    - Add new icon    - Remove unused icons

---
+++ docs/introduction.md
@@ -0,0 +1,35 @@
+---
+sidebar_position: 1
+title: Introduction
+---
+
+# Welcome to Text-ssg Docs
+
+Hello! Thanks for stopping by. Text ssg is a Static Site Generator in CLI that convert .txt and .md files into .html files.
+
+## Author
+
+👨‍💻 **Kevan Yang**
+
+- Github: [@Kevan-Y](https://github.com/Kevan-Y)
+
+## Contributors
+
+<!-- |!(https://avatars.githubusercontent.com/u/58233223?s=60&v=4)| -->
+<!-- markdownlint-disable -->
+<table>
+  <tbody>
+    <tr>
+      <td align="center"><a href="https://github.com/Kevan-Y"><img src="https://avatars.githubusercontent.com/u/58233223?v=3?s=100" width="100px;" alt=""/><br /><sub><b>Kevan-Y</b></sub></a>
+      </td>
+      <td align="center"><a href="https://github.com/irenejoeunpark"><img src="https://avatars.githubusercontent.com/u/54859944?v=3?s=100" width="100px;" alt=""/><br /><sub><b>irenejoeunpark</b></sub></a>
+      </td>
+      <td align="center"><a href="https://github.com/DukeManh"><img src="https://avatars.githubusercontent.com/u/51073515?v=3?s=100" width="100px;" alt=""/><br /><sub><b>DukeManh</b></sub></a>
+      </td>
+    </tr>
+  </tbody>
+</table>
+
+## Show your support
+
+Give a ⭐️ to this [project](https://github.com/Kevan-Y/text-ssg).
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,113 +6,107 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+	title: 'Text-ssg docs',
+	tagline: 'Dinosaurs are cool',
+	url: 'https://Kevan-Y.github.io',
+	baseUrl: '/text-ssg-docs/',
+	onBrokenLinks: 'throw',
+	onBrokenMarkdownLinks: 'warn',
+	favicon: 'img/favicon.ico',
+	organizationName: 'kevan-y', // Usually your GitHub org/user name.
+	projectName: 'text-ssg-docs', // Usually your repo name.
 
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
-    ],
-  ],
+	presets: [
+		[
+			'@docusaurus/preset-classic',
+			/** @type {import('@docusaurus/preset-classic').Options} */
+			({
+				docs: {
+					sidebarPath: require.resolve('./sidebars.js'),
+					// Please change this to your repo.
+					editUrl: 'https://github.com/Kevan-Y/text-ssg-docs/edit/main/docs',
+				},
+				blog: {
+					showReadingTime: true,
+					// Please change this to your repo.
+					editUrl: 'https://github.com/Kevan-Y/text-ssg-docs/edit/main/blog/',
+				},
+				theme: {
+					customCss: require.resolve('./src/css/custom.css'),
+				},
+			}),
+		],
+	],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'My Site',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+	themeConfig:
+		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+		({
+			navbar: {
+				title: 'Text ssg',
+				logo: {
+					alt: 'My Site Logo',
+					src: 'img/logo.png',
+				},
+				items: [
+					{
+						type: 'doc',
+						docId: 'introduction',
+						position: 'left',
+						label: 'Docs',
+					},
+					{ to: '/blog', label: 'Blog', position: 'left' },
+					{
+						href: 'https://github.com/Kevan-Y/text-ssg',
+						label: 'GitHub',
+						position: 'right',
+					},
+				],
+			},
+			footer: {
+				style: 'dark',
+				links: [
+					{
+						title: 'Docs',
+						items: [
+							{
+								label: 'Introduction',
+								to: '/docs/introduction',
+							},
+							{
+								label: 'Installation',
+								to: '/docs/installation',
+							},
+							{
+								label: 'Usage',
+								to: '/docs/usage',
+							},
+							{
+								label: 'Example',
+								to: '/docs/example',
+							},
+						],
+					},
+					{
+						title: 'More',
+						items: [
+							{
+								label: 'Blog',
+								to: '/blog',
+							},
+							{
+								label: 'GitHub',
+								href: 'https://github.com/Kevan-Y/text-ssg',
+							},
+						],
+					},
+				],
+				copyright: `Copyright © ${new Date().getFullYear()} Text-ssg, Inc. Built with Docusaurus.`,
+			},
+			prism: {
+				theme: lightCodeTheme,
+				darkTheme: darkCodeTheme,
+			},
+		}),
 };
 
 module.exports = config;
